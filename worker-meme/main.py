3c8052a495e9467d1eea94d638f6d74f9696ba95
hiephtdev <work.hiepht@gmail.com>
2024-07-26 15:45:18 +0700

    xoa key

--- worker-meme/main.py
+++ worker-meme/main.py
@@ -2,7 +2,7 @@ import requests
 import sys
 import json
 
-API_KEY = 'UP-9c8c9439cc87403a8ff447f5'  # Replace with your actual API key
+API_KEY = 'UP-'  # Replace with your actual API key
 
 def get_token_details(block_height):
     url = f"https://api.upshot.xyz/v2/allora/tokens-oracle/token/{block_height}"
